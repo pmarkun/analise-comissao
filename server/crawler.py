9ee0af5cf5e3d88a8e0ef645e6792cfc31cb3f12
Pedro Markun <pedro@esfera.mobi>
2013-03-08 09:59:53 -0300

    desisti de usar o python como server, vamos consumir JSON diretamente e atualizar o crawler em um determinado intervalo de tempo. Agora ele gera JSON de todas as comissoes

--- server/crawler.py
+++ server/crawler.py
@@ -1,19 +1,22 @@
+# -*- coding: utf-8 -*-
 from lxml.etree import parse
 import subprocess
 import json
 
 def loadOrdownload(url, filename):
+    """Tenta abrir o arquivo (filename) se não conseguir, baixa da (url) com wget."""
     try:
-        arquivo = open("data/" + filename, "r")
+        arquivo = open("../data/" + filename, "r")
         print filename + " file found... loading..."
         return arquivo
     except:
         print filename + " file not found... getting online..."
         subprocess.call(["wget",url,"-O","data/"+filename])
-        arquivo = open("data/"+filename, "r")
+        arquivo = open("../data/"+filename, "r")
         return arquivo
     
 def carregaDeputados():
+    """Carrega o arquivo XML com os deputados em exercício e extrai os campos necessários"""
     deputados = loadOrdownload("http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDeputados","deputados.xml")
     soup = parse(deputados)
     lista_deputados = {}
@@ -33,6 +36,7 @@ def carregaDeputados():
     return lista_deputados
 
 def carregaProposicoes(ano, tipo, lista_deputados):
+    """Carrega o arquivo XML com as proposições de um determinado tipo e ano e monta uma lista por deputado"""
     proposicoes = loadOrdownload("http://www.camara.gov.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoes?sigla="+tipo+"&numero=&ano="+ano+"&datApresentacaoIni=&datApresentacaoFim=&autor=&parteNomeAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao=", tipo+"_"+ano+".xml")
     soup = parse(proposicoes)
 
@@ -49,17 +53,40 @@ def carregaProposicoes(ano, tipo, lista_deputados):
             print proposicao
     return lista_deputados
 
-lista_deputados = carregaDeputados()
-lista_deputados = carregaProposicoes("2011","PL",lista_deputados)
-lista_deputados = carregaProposicoes("2012","PL",lista_deputados)
-lista_deputados = carregaProposicoes("2011","PEC",lista_deputados)
-lista_deputados = carregaProposicoes("2012","PEC",lista_deputados)
+def preparaLista():
+    """Prepara a lista de deputados e proposições"""
+    tipos = ["PL","PEC"]
+    anos = ["2011", "2012"]
+        
 
-lista_simples = []
-for deputado in lista_deputados:
-    if "CDHM" in lista_deputados[deputado]["comissoes"]:
-        lista_simples.append(lista_deputados[deputado])
+    lista_deputados = carregaDeputados()    
+    for tipo in tipos:
+        for ano in anos:
+            lista_deputados = carregaProposicoes(ano, tipo,lista_deputados)
+    return lista_deputados
+    
 
-cdhm = open("cdhm.json", "w")
-cdhm.write(json.dumps(lista_simples, sort_keys=True, indent=4, separators=(',', ': ')));
-cdhm.close()
+def listaComissoes(lista_deputados):
+    """Cria uma lista com todas as comissoes"""
+    comissoes = []
+    for deputado in lista_deputados:
+        comissoes = comissoes + lista_deputados[deputado]["comissoes"]
+    comissoes = list(set(comissoes))
+    return comissoes
+
+def escreveDados():
+    """Escreve os arquivos JSON das comissões na pasta publica"""
+    lista_deputados = preparaLista()
+    comissoes = listaComissoes(lista_deputados)
+    for comissao in comissoes:
+        lista_simples = []
+        for deputado in lista_deputados:
+            if comissao in lista_deputados[deputado]["comissoes"]:
+                lista_simples.append(lista_deputados[deputado])
+
+        comissao = open("../public/data/"+comissao+".json", "w")
+        comissao.write(json.dumps(lista_simples, sort_keys=True, indent=4, separators=(',', ': ')));
+        comissao.close()
+
+
+escreveDados()
